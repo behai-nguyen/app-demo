e2b3c0bd2307f4baa3cb0d471ab0c2d817aad8d6
behai-nguyen <behai_nguyen@hotmail.com>
2022-08-06 10:48:41 +1000

    Blueprint and route registration. Working version.

---
+++ src/app_demo/urls.py
@@ -0,0 +1,23 @@
+"""
+Routes mappers.
+"""
+from app_demo.controllers import ( 
+    echo_blueprint,
+	echo,
+)
+
+"""
+https://stackoverflow.com/questions/33241050/trailing-slash-triggers-404-in-flask-path-rule
+Trailing slash triggers 404 in Flask path rule
+
+url_map.strict_slashes = False 
+
+@bp.route('/my-route', methods=['POST'])
+
+/my-route and /my-route/ both work identically.
+"""
+urls = [
+    ( '/echo', [ 'GET', 'POST' ], echo_blueprint, echo.do_echo ),
+]
+
+blueprints = [ echo_blueprint ]
